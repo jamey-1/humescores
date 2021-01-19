f71a225062d014fc5ddff4dd755764520ca46d93
YEMING LIU <liuyeming@zhouyang.net>
2021-01-19 16:38:22 +0800

    init commit

---
+++ footer.php
@@ -0,0 +1,45 @@
+<?php
+/**
+ * The template for displaying the footer
+ *
+ * Contains the closing of the #content div and all content after.
+ *
+ * @link https://developer.wordpress.org/themes/basics/template-files/#template-partials
+ *
+ * @package Humescores
+ */
+
+?>
+
+	<footer id="colophon" class="site-footer">
+		<nav class="social-menu">
+		<?php
+			wp_nav_menu(
+				array(
+					'theme_location' => 'menu-2',
+					'menu_id'        => 'footer-menu',
+				)
+			);
+		?>
+		</nav>
+
+		<div class="site-info">
+			<a href="<?php echo esc_url( __( 'https://wordpress.org/', 'humescores' ) ); ?>">
+				<?php
+				/* translators: %s: CMS name, i.e. WordPress. */
+				printf( esc_html__( 'Proudly powered by %s', 'humescores' ), 'WordPress' );
+				?>
+			</a>
+			<span class="sep"> | </span>
+				<?php
+				/* translators: 1: Theme name, 2: Theme author. */
+				printf( esc_html__( 'Theme: %1$s by %2$s.', 'humescores' ), 'humescores', '<a href="https://jamesliu666.com">James Liu</a>' );
+				?>
+		</div><!-- .site-info -->
+	</footer><!-- #colophon -->
+</div><!-- #page -->
+
+<?php wp_footer(); ?>
+
+</body>
+</html>
